1bfb22e7b88ba59c566a60b0c92b4346c087a686
Youssef Mohammed <youssefmohammed2093@gmail.com>
2024-08-01 16:38:37 +0300

    start

---
+++ src/app/(auth)/signup/actions.ts
@@ -0,0 +1,75 @@
+"use server";
+
+import { lucia } from "@/auth";
+import prisma from "@/lib/prisma";
+import { signUpSchema, SignUpValues } from "@/lib/validation";
+import { hash } from "@node-rs/argon2";
+import { generateIdFromEntropySize } from "lucia";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function signUp(
+  credentials: SignUpValues
+): Promise<{ error: string }> {
+  try {
+    const { username, email, password } = signUpSchema.parse(credentials);
+    const passwordHash = await hash(password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    });
+    const userId = generateIdFromEntropySize(10);
+    const session = await lucia.createSession(userId, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes
+    );
+    const existingUsername = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
+      },
+    });
+    const existingEmail = await prisma.user.findFirst({
+      where: {
+        email: {
+          equals: email,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingUsername) {
+      return {
+        error: "This username already taken!",
+      };
+    }
+    if (existingEmail) {
+      return {
+        error: "This email already taken!",
+      };
+    }
+
+    await prisma.user.create({
+      data: {
+        id: userId,
+        username,
+        displayName: username,
+        email,
+        passwordHash,
+      },
+    });
+
+    return redirect("/");
+  } catch (err) {
+    console.error(err);
+    return {
+      error: "Something Went Wrong, Please Try Again.",
+    };
+  }
+}
