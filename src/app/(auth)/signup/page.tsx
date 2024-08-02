727dd1dca0be77a863dcac8f866da29585c133eb
Youssef Mohammed <youssefmohammed2093@gmail.com>
2024-08-02 12:28:06 +0300

    sign up page complete

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import SignUpImg from "@/assets/signup-image.jpg";
+import SignUpForm from "./signUpForm";
 
 export const metadata: Metadata = {
   title: "Sign Up",
@@ -10,18 +11,26 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="flex items-center justify-center h-screen p-5">
-      <div className="flex w-full h-full max-w-[64rem] max-h-[32rem] overflow-hidden rounded-xl shadow-xl bg-card">
+      <div
+        className="flex w-full h-full max-w-[64rem] max-h-[34rem] overflow-hidden rounded-xl shadow-xl bg-card"
+        style={{ border: "1px solid #e2e8f0" }}
+      >
         <div className="w-full md:w-1/2 space-y-10 overflow-y-auto p-5">
           <div className="text-center space-y-1">
-            <h1 className="text-3xl font-bold">Sign Up To Matrix App</h1>
+            <h1 className="text-2xl sm:text-3xl font-bold">
+              Sign Up To Matrix App
+            </h1>
             <p className="text-muted-foreground pt-3">
               A place where even <span className="italic">you</span> can find a
               friend.
             </p>
           </div>
           <div>
-            <div className="text-center bg-blue-500">Sign Up Form Here!</div>
-            <Link href="/login" className="block text-center hover:underline">
+            <SignUpForm />
+            <Link
+              href="/login"
+              className="block text-center hover:underline mt-8"
+            >
               Already have an account? Log in
             </Link>
           </div>
